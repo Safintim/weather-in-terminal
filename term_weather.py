817ce64d10a7a89af5c654ed5cbcb8e156374990
Safintim <Sarami96>
2019-04-10 16:15:37 +0300

    almost ready

---
+++ term_weather.py
@@ -0,0 +1,24 @@
+import requests
+
+OFFICES = ['Kazan', 'Moscow', 'Samara']
+URL_TEMPLATE = 'http://wttr.in/{}'
+PARAMS = {
+    'lang': 'ru',
+    'm': ''
+}
+
+
+def get_weather(office=None):
+    if office:
+        url = URL_TEMPLATE.format(office)
+        req = requests.get(url, params=PARAMS)
+        print(req.text)
+    else:
+        for office in OFFICES:
+            url = URL_TEMPLATE.format(office)
+            req = requests.get(url, params=PARAMS)
+            print(req.text)
+
+
+if __name__ == '__main__':
+    get_weather()

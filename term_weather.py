4c1a78b8032233249bd9ab593e0c74ff7dac433f
Safintim <Sarami96>
2019-04-11 15:10:02 +0300

    add command line interface

--- term_weather.py
+++ term_weather.py
@@ -1,4 +1,6 @@
 import requests
+import argparse
+
 
 OFFICES = ['Kazan', 'Moscow', 'Samara']
 URL_TEMPLATE = 'http://wttr.in/{}'
@@ -11,17 +13,22 @@ PARAMS = {
 }
 
 
-def get_weather(office=None):
-    if office:
+def get_weather():
+    for office in OFFICES:
         url = URL_TEMPLATE.format(office)
         req = requests.get(url, params=PARAMS)
         print(req.text)
-    else:
-        for office in OFFICES:
-            url = URL_TEMPLATE.format(office)
-            req = requests.get(url, params=PARAMS)
-            print(req.text)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--city', nargs='+', default=['Kazan'])
+    return parser
 
 
 if __name__ == '__main__':
+    parser = create_parser()
+    namespace = parser.parse_args()
+    OFFICES = namespace.city
     get_weather()
+
